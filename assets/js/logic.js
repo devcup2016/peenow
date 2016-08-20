a7c6f5c5f61faab58b7e14d3944a45d9bd181e43
amielsuarez1 <amiel.suarez@nuworks.ph>
2016-08-20 18:35:36 +0800

    added camera

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,7 +1,53 @@
+alert("run now2")
+var geolocation_ready = function () {
+    setTimeout(function(){
+        try{
+            alert(navigator.geolocation)
+            navigator.geolocation.getCurrentPosition(disp);
+        } catch(e){
+            var error = document.createElement("div")
+            error.innerHTML = e;
+            document.body.appendChild(error)
+            geolocation_ready()
+        }
+    },500)
+}
+
+var camera_ready = function () {
+    setTimeout(function(){
+        try{
+            alert(navigator.camera)
+            navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+                destinationType: Camera.DestinationType.DATA_URL
+            });
+        } catch(e){
+            var error = document.createElement("div")
+            error.innerHTML = e;
+            document.body.appendChild(error)
+            camera_ready()
+        }
+    },500)
+}
+
+
+camera_ready()
+geolocation_ready()
+
+
+
 function disp(pos) {
    var lat = pos.coords.latitude;
    var long = pos.coords.longitude;
-   // alert(lat + ", " + long);
+   alert(lat + ", " + long);
 }
 
-navigator.geolocation.getCurrentPosition(disp);
+
+
+function onSuccess(imageData) {
+    var image = document.getElementById('myImage');
+    image.src = "data:image/jpeg;base64," + imageData;
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
